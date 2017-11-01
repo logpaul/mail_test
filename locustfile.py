924a8b36d07d80ffaefe4e7d562bbcd2a9c53f25
Paul Logachev <logachevpa@gmail.com>
2017-11-01 23:54:06 +0300

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,12 +1,10 @@
 from locust import HttpLocust, TaskSet
 import resource
 resource.setrlimit(resource.RLIMIT_NOFILE, (999999, 999999))
-# def login(l):
-#     l.client.post("/login", {"username":"ellen_key", "password":"education"})
+
 
 def all_articles(l):
     l.client.get("/articles/?format=json")
-
 
 def get_user_articles(l):
     l.client.get("/user/1/articles?format=json")
